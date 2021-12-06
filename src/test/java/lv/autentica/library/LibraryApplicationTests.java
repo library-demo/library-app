086fb3649a316d35b6be2ed74db839e0ec0c7cbf
Natalija Dubenko <natalija.dubenko@autentica.lv>
2021-12-06 14:52:53 +0200

    Initial commit

---
+++ src/test/java/lv/autentica/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.autentica.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
